2bb192fdaba58063ca4d96385f02ad67ec0e9672
the-tanvan <alyjen.tan@gmail.com>
2020-10-05 22:42:32 -0400

    Added cleaning of cleaning_fee

--- tidy_data.R
+++ tidy_data.R
@@ -2,5 +2,7 @@ library(tidyverse)
 listings = read.csv("source_data/listings.csv")
 
 listings$price = as.numeric(sub('.', '', listings$price))
+listings$cleaning_fee = as.numeric(sub('.', '', listings$cleaning_fee))
+listings = listings %>% mutate(total_price = price + cleaning_fee) %>% filter(!is.na(total_price))
 
 write_csv(listings, "derived_data/listings.csv")
