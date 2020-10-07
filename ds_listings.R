e41729de9d778ca250c6ccb2e529405f16901626
the-tanvan <alyjen.tan@gmail.com>
2020-10-07 00:59:29 -0400

    Added intro code block

--- ds_listings.R
+++ ds_listings.R
@@ -1,5 +1,9 @@
+# Program Name: ds_listings.R
+# Purpose: This creates the .csv of the listings dataset based on the source data. 
+# It is tidied up by changing some variables into numeric form and keeping only obs with total_price (our derived outcome variable).
+
 library(tidyverse)
-listings = read.csv("source_data/listings.csv")
+listings=read.csv("source_data/listings.csv")
 
 listings$price = as.numeric(sub('.', '', listings$price))
 listings$cleaning_fee = as.numeric(sub('.', '', listings$cleaning_fee))
