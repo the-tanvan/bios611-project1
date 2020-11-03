f827cc2de5a1980b438e56bc946d1c8b1a47a7b9
the-tanvan <alyjen.tan@gmail.com>
2020-11-03 13:10:12 -0500

    Added script for amenities.csv

---
+++ ds_amenities.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("derived_data/listings.csv")
+row_total = df.shape[0]
+
+df.loc[df['amenities'].str.contains('Wifi'),'wifi'] = 'True'
+wifi_count=df.wifi.value_counts()
+
+df.loc[df['amenities'].str.contains('Stove'),'stove'] = 'True'
+stove_count=df.stove.value_counts()
+
+df.loc[df['amenities'].str.contains('Hot tub'),'hottub'] = 'True'
+hottub_count=df.hottub.value_counts()
+
+df.loc[(df['wifi']=="True") & (df['stove']=="True") & (df['hottub']=="True"),'ideal'] = 'True'
+ideal_count=df.ideal.value_counts()
+
+df = df[["wifi","stove","hottub","ideal"]]
+analysis = pd.get_dummies(df)
+np.savetxt("derived_data/amenities.csv", analysis, delimiter=",")
